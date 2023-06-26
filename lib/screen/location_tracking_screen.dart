0ce6eeb98b05c07ba8b096b43c4072c43e603f6e
tuantm2703 <tuantmd2703@gmail.com>
2023-06-26 09:01:30 +0700

    drawing route between markers

--- lib/screen/location_tracking_screen.dart
+++ lib/screen/location_tracking_screen.dart
@@ -15,14 +15,72 @@ class LocationTrackingScreen extends StatefulWidget {
 }
 
 class _LocationTrackingScreenState extends State<LocationTrackingScreen> {
+  final Completer<GoogleMapController> mapCompleter = Completer();
+  LatLng? sourceLocation, destinationLocation;
+  List<LatLng> polylineCoordinates = [];
 
   @override
   void initState() {
+    initLocation();
+    getPolyPoints();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: (sourceLocation == null && destinationLocation == null)
+          ? const Text('Loading')
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: sourceLocation!,
+                zoom: 13.5,
+              ),
+              markers: {
+                Marker(
+                  markerId: const MarkerId('source'),
+                  position: sourceLocation!,
+                ),
+                Marker(
+                  markerId: const MarkerId('destination'),
+                  position: destinationLocation!,
+                ),
+              },
+              onMapCreated: (mapController) {
+                mapCompleter.complete(mapController);
+              },
+              polylines: {
+                Polyline(
+                  polylineId: const PolylineId('route'),
+                  points: polylineCoordinates,
+                  color: Colors.blue,
+                  width: 4,
+                ),
+              },
+            ),
+    );
+  }
+
+  void initLocation() {
+    sourceLocation = const LatLng(37.33500926, -122.03272188);
+    destinationLocation = const LatLng(37.33429383, -122.06600055);
+    printDebug('Source: - Lat:${sourceLocation?.latitude} - Long:${sourceLocation?.longitude}');
+    printDebug('Destination: - Lat:${destinationLocation?.latitude} - Long:${destinationLocation?.longitude}');
+  }
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    if (sourceLocation != null && destinationLocation != null) {
+      PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        AppConfig.googleApiKey,
+        PointLatLng(sourceLocation!.latitude, sourceLocation!.longitude),
+        PointLatLng(destinationLocation!.latitude, destinationLocation!.longitude),
+      );
+      polylineCoordinates.clear();
+      if (result.points.isNotEmpty) {
+        polylineCoordinates = [...result.points.map((e) => LatLng(e.latitude, e.longitude)).toList()];
+        setState(() {});
+      }
+    }
   }
 }
